86ee8b127e4a5682a8b3cfe2a21e96e9a81e03ae
PP_Pro <chinese1236@qq.com>
2022-08-23 17:19:28 +0800

    add type declaration

---
+++ @types/hmr.d.ts
@@ -0,0 +1,44 @@
+
+type DisposeHandler = (data: unknown) => void;
+type ErrorHandler = (err: unknown, res: { moduleId: string, dependencyId: string }) => void
+
+
+type ModuleId = string;
+type Module = Object;
+type ModuleMap = Record<ModuleId, Module>;
+
+declare class ModuleSystem {
+
+    set defaultHotupdatable (v: boolean);
+    get defaultHotupdatable (): boolean;
+
+    public getModules (): ModuleMap;
+    public reload (modules: string[]): Promise<boolean>;
+}
+
+declare global {
+    interface ImportMeta {
+        ccHot?: {
+            data: unknown;
+
+            accept(errorHandler?: ErrorHandler): void;
+            accept(
+                modules: string | string[],
+                callback: () => void,
+                errorHandler?: ErrorHandler,
+            ): void;
+
+            
+            decline(): void;
+            decline(dependencies: string[]): void;
+
+            dispose(handler: DisposeHandler);
+
+            upvalue<TFunction extends Function> (target: TFunction): TFunction | void;
+        };
+
+        moduleSystem?: ModuleSystem;
+    }
+}
+
+export {};
